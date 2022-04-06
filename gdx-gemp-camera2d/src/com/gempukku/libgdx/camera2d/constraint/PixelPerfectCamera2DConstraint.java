f44b43463ce2eaee09f27f54c75ad4e1072c71a2
MarcinSc <marcins78@gmail.com>
2022-04-06 18:54:19 +0700

    Fixed Pixel Perfect, in case of odd viewport width or height

--- gdx-gemp-camera2d/src/com/gempukku/libgdx/camera2d/constraint/PixelPerfectCamera2DConstraint.java
+++ gdx-gemp-camera2d/src/com/gempukku/libgdx/camera2d/constraint/PixelPerfectCamera2DConstraint.java
@@ -11,6 +11,12 @@ public class PixelPerfectCamera2DConstraint implements Camera2DConstraint {
         camera.position.y = MathUtils.round(camera.position.y);
         camera.viewportWidth = MathUtils.round(camera.viewportWidth);
         camera.viewportHeight = MathUtils.round(camera.viewportHeight);
+
+        if (MathUtils.round(camera.viewportWidth) % 2 == 1)
+            camera.position.x -= 0.5f;
+        if (MathUtils.round(camera.viewportHeight) % 2 == 1)
+            camera.position.y -= 0.5f;
+
         camera.update();
     }
 }
