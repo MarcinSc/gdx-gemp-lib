573f8394dd6b82c27c730e682d56717bff2298eb
MarcinSc <marcins78@gmail.com>
2022-12-30 04:12:13 +0700

    Removed the obsolete Alignment class

--- gdx-gemp-camera2d/src/com/gempukku/libgdx/camera2d/constraint/AlignUnderflowCamera2DConstraint.java
+++ gdx-gemp-camera2d/src/com/gempukku/libgdx/camera2d/constraint/AlignUnderflowCamera2DConstraint.java
@@ -3,13 +3,13 @@ package com.gempukku.libgdx.camera2d.constraint;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
-import com.gempukku.libgdx.util.Alignment;
+import com.badlogic.gdx.utils.Align;
 
 public class AlignUnderflowCamera2DConstraint implements Camera2DConstraint {
     private Rectangle bounds;
-    private Alignment alignment;
+    private int alignment;
 
-    public AlignUnderflowCamera2DConstraint(Rectangle bounds, Alignment alignment) {
+    public AlignUnderflowCamera2DConstraint(Rectangle bounds, int alignment) {
         this.bounds = bounds;
         this.alignment = alignment;
     }
@@ -18,7 +18,7 @@ public class AlignUnderflowCamera2DConstraint implements Camera2DConstraint {
         this.bounds.set(bounds);
     }
 
-    public void setAlignment(Alignment alignment) {
+    public void setAlignment(int alignment) {
         this.alignment = alignment;
     }
 
@@ -28,11 +28,19 @@ public class AlignUnderflowCamera2DConstraint implements Camera2DConstraint {
         float previousY = camera.position.y;
 
         if (camera.viewportWidth > bounds.width)
-            camera.position.x = bounds.x + alignment.applyX(camera.viewportWidth, bounds.width) + camera.viewportWidth / 2;
+            camera.position.x = bounds.x + applyX(camera.viewportWidth, bounds.width) + camera.viewportWidth / 2;
         if (camera.viewportHeight > bounds.height)
-            camera.position.y = bounds.y + alignment.applyY(camera.viewportHeight, bounds.height) + camera.viewportHeight / 2;
+            camera.position.y = bounds.y + applyY(camera.viewportHeight, bounds.height) + camera.viewportHeight / 2;
 
         if (camera.position.x != previousX || camera.position.y != previousY)
             camera.update();
+    }
+
+    private float applyX(float width, float availableWidth) {
+        return Align.isLeft(alignment) ? 0 : (Align.isRight(alignment) ? (availableWidth - width) : ((availableWidth - width) / 2));
+    }
+
+    private float applyY(float height, float availableHeight) {
+        return Align.isTop(alignment) ? 0 : (Align.isBottom(alignment) ? (availableHeight - height) : ((availableHeight - height) / 2));
     }
 }
