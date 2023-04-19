af1247dc5d6ef5467c18544a58905953e4f22e40
MarcinSc <marcins78@gmail.com>
2023-04-19 13:11:10 +0700

    Fixed dot after end of number with 0 decimals after period

--- gdx-gemp-common/src/com/gempukku/libgdx/common/SimpleNumberFormatter.java
+++ gdx-gemp-common/src/com/gempukku/libgdx/common/SimpleNumberFormatter.java
@@ -1,7 +1,7 @@
 package com.gempukku.libgdx.common;
 
 public class SimpleNumberFormatter {
-    public static String format(float value, int decimalsAfterZero) {
+    public static String format(float value, int decimalsAfterPeriod) {
         StringBuilder result = new StringBuilder();
         if (value < 0f) {
             result.append('-');
@@ -9,13 +9,15 @@ public class SimpleNumberFormatter {
         }
         result.append((int) value);
         value -= ((int) value);
-        result.append(".");
-        for (int i = 0; i < decimalsAfterZero; i++) {
-            if (i > 0 && value == 0f)
-                break;
-            value *= 10f;
-            result.append((int) value);
-            value -= ((int) value);
+        if (value != 0f && decimalsAfterPeriod > 0) {
+            result.append(".");
+            for (int i = 0; i < decimalsAfterPeriod; i++) {
+                if (i > 0 && value == 0f)
+                    break;
+                value *= 10f;
+                result.append((int) value);
+                value -= ((int) value);
+            }
         }
         return result.toString();
     }
